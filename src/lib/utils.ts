0f4f4ae2f99b75c84d1081f9437e93ad287f9e7b
Nizar <hussainnizar747@yahoo.com>
2025-12-19 20:18:08 -0500

    Fix: Improve utils.ts with JSDoc and explicit return type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Utility function to merge Tailwind CSS classes
+ * Combines clsx and tailwind-merge for optimal class merging
+ * 
+ * @param inputs - Class values to merge
+ * @returns Merged class string
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
-
